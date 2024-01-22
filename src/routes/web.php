3e1ed4c1b399c8769888fdfdfe0c8d3d30e4d88f
koki waku <kokiwaku1212@gmail.com>
2024-01-22 09:58:10 +0900

    [mod]カーソルベースのページネーションを手動実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,11 +2,13 @@
 
 use App\Models\Posts;
 use Illuminate\Database\DatabaseManager;
+use Illuminate\Pagination\Cursor;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\CursorPaginator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,10 +23,12 @@ use Illuminate\Http\Request;
 
 Route::get('/offset', function(DatabaseManager $databaseManager) {
 
+    // for debug
     $databaseManager->enableQueryLog();
 
     $postsList = Posts::query()->orderBy('posts_id', 'asc')->paginate(5);
 
+    // for debug
     $query = $databaseManager->getQueryLog();
 
     return view('posts.index', compact('postsList'));
@@ -72,10 +76,70 @@ Route::get('/offset/manual', function(Request $request, DatabaseManager $databas
 
 Route::get('/cursor', function(DatabaseManager $databaseManager) {
 
+    // for debug
     $databaseManager->enableQueryLog();
 
     $postsList = Posts::query()->orderBy('posts_id', 'asc')->cursorPaginate(5);
 
+    // for debug
+    $query = $databaseManager->getQueryLog();
+
+    return view('posts.index', compact('postsList'));
+});
+
+Route::get('/cursor/manual', function(Request $request, DatabaseManager $databaseManager) {
+
+    // for debug
+    $databaseManager->enableQueryLog();
+
+    // get base query
+    $query = Posts::query();
+
+    // perPage(limit)
+    $perPage = 5;
+    $query->limit($perPage + 1);
+
+    // where、orderBy（cursor）
+    $cursorParam = $request->input('cursor');
+    if ($cursorParam === null) {
+        $query->orderBy('posts_id', 'asc');
+    } else {
+        // cursor
+        $cursorEncoded = Cursor::fromEncoded($cursorParam);
+        $cursor = new Cursor(
+            parameters: ['posts_id' => $cursorEncoded->parameter('posts_id')],
+            pointsToNextItems: $cursorEncoded->pointsToNextItems(),
+        );
+
+        // where、orderBy
+        if ($cursor->pointsToNextItems()) {
+            $orderBy = 'asc';
+            $operator = '>';
+        } else {
+            $orderBy = 'desc';
+            $operator = '<';
+        }
+        $query->orderBy('posts_id', $orderBy);
+        $query->where('posts_id', $operator, $cursor->parameter('posts_id'));
+    }
+    // execute
+    $items = $query->get();
+
+    // generate paginator
+    $postsList = new CursorPaginator(
+        items: $items,
+        perPage: $perPage,
+        cursor: $cursor ?? null,
+        options: [
+            'path' => Paginator::resolveCurrentPath(),
+            'corsorName' => 'cursor',
+            'parameters' => [
+                'posts_id',
+            ],
+        ]
+    );
+
+    // for debug
     $query = $databaseManager->getQueryLog();
 
     return view('posts.index', compact('postsList'));
